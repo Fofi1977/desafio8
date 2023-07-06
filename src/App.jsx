ac7633d39e221d0e85c7a67a78de30439b06cbb9
Fofi1977 <sofiaaquino014@gmail.com>
2023-07-06 13:12:18 -0300

    comentario

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Desafio8 from './desafio8'
+
+function App() {
+  
+  return (
+    
+      <div>
+        <Desafio8/>
+      </div>
+    
+  )
+}
+
+export default App
